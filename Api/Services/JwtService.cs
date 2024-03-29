da9146c5bdd94152de55311f166f1bdce7902687
Hussein <41930483@students.liu.edu.lb>
2024-03-29 16:19:53 +0200

    Add JWT token generation to JwtService.cs

--- Api/Services/JwtService.cs
+++ Api/Services/JwtService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
@@ -25,8 +26,20 @@ namespace Api.Services
                 new Claim(ClaimTypes.NameIdentifier,user.Id),
                 new Claim(ClaimTypes.Email,user.Email),
                 new Claim(ClaimTypes.GivenName,user.FirstName),
-
-            }
+                new Claim(ClaimTypes.Surname,user.LastName)
+            };
+             var credientials = new SigningCredentials(_jwtkey,SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(Userclaims),
+                Expires = System.DateTime.Now.AddDays(int.Parse(configuration["Jwt:ExpiresInDays"])),
+                SigningCredentials = credientials,
+                Issuer = configuration["Jwt:Issuer"]
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+            
         }
     }
 }
