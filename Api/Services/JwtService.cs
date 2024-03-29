92b7a970face2dcaee0a56e5fbaaec02870177b8
Hussein <41930483@students.liu.edu.lb>
2024-03-29 16:01:16 +0200

    Add JwtService class for generating JWT tokens

---
+++ Api/Services/JwtService.cs
@@ -0,0 +1,32 @@
+﻿using Api.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Api.Services
+{
+    public class JwtService
+    {
+        private readonly IConfiguration configuration;
+        private readonly SymmetricSecurityKey _jwtkey;
+
+        public JwtService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            _jwtkey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+        }
+        public string GenerateJWT(Users user)
+        {
+            var Userclaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Id),
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.GivenName,user.FirstName),
+
+            }
+        }
+    }
+}
