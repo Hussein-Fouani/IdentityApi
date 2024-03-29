3316f137bb458526dc2a9cc6b25067db9483cb62
Hussein <41930483@students.liu.edu.lb>
2024-03-29 14:24:47 +0200

    Add Users model with properties

---
+++ Api/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models
+{
+    public class Users:IdentityUser
+    {
+        [Required]
+        [PersonalData]
+        public string FirstName { get; set; }
+        [Required]
+        [PersonalData]
+        public string LastName { get; set; }
+        public DateTime DateCreated{ get; set; } = DateTime.Now;
+    }
+}
