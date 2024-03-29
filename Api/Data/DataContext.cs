946119f16bc9d261f93ceb6d3c4d161653b7ef26
Hussein <41930483@students.liu.edu.lb>
2024-03-29 14:24:40 +0200

    Add DataContext class to Api.Data

---
+++ Api/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class DataContext:IdentityDbContext<Users>
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+        }
+        public DbSet<Users> Users { get; set; }
+    }
+}
