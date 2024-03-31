7d4e78c6cfc314d6fcf2ce565ff16cd9801fb3b6
Hussein <41930483@students.liu.edu.lb>
2024-03-31 17:52:09 +0300

    Add route for PlayComponent

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import path from 'node:path';
 import { HomeComponent } from './home/home.component';
+import { PlayComponent } from './play/play.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'play', component: PlayComponent },
   { path: 'not-found', component: NotfoundComponent },
   { path: '**', component: NotfoundComponent, pathMatch: 'full' },
   {
