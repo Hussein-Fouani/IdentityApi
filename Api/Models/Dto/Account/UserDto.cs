1dc25748acbdd1657ee3a753efb4ddf6d1dec5cb
Hussein <41930483@students.liu.edu.lb>
2024-03-31 14:46:10 +0300

    Add UserDto class

---
+++ Api/Models/Dto/Account/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Models.Dto.Account
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Jwt { get; set; }
+    }
+}
