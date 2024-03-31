41d90db06553a1e6dfb152670abc937cc569cf59
Hussein <41930483@students.liu.edu.lb>
2024-03-31 14:45:52 +0300

    Add LoginDto class for account login

---
+++ Api/Models/Dto/Account/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models.Dto.Account
+{
+    public class LoginDto
+    {
+        [Required]
+
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
